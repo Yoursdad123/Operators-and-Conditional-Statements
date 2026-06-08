35a16c91642991895b741f9059360272e987ce75
Yoursdad123 <rudrahon3@gmail.com>
2026-06-08 20:48:50 +0530

    Create membership.py

---
+++ membership.py
@@ -0,0 +1,45 @@
+1. Student search system
+
+students = ["Rudra", "Amit", "Rahul"]
+name = input("Enter Student Name: ")
+if name in students:
+    print("Student Found")
+else:
+    print("Student Not Found")
+
+2. Employee record search
+
+employees = ["E101", "E102", "E103"]
+eid = input("Enter Employee ID: ")
+if e id in employees:
+    print("Employee Found")
+else:
+    print("Employee Not Found")
+
+3. Product Availability checker 
+
+products = ["Laptop", "Mouse", "Keyboard"]
+item = input("Enter Product: ")
+if item in products:
+    print("Available")
+else:
+    print("Not Available")
+
+4. Library book search 
+
+books = ["Python", "Java", "C++"]
+book = input("Enter Book Name: ")
+if book in books:
+    print("Book Available")
+else:
+    print("Book Not Available")
+
+5. Course Enrollment verification 
+
+courses = ["Python", "DBMS", "Web Design"]
+course = input("Enter Course Name: ")
+if course in courses:
+    print("Enrolled")
+else:
+    print("Not Enrolled")
+
